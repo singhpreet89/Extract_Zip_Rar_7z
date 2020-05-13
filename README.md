57f5b3c5e0bf365d5685d8bf8bc847701bac4e97
Harpreet Singh <singh.harpreet89@hotmail.com>
2020-05-13 01:35:17 -0400

    Added .7z extraction and updated the code

--- README.md
+++ README.md
@@ -1,44 +1,48 @@
 # UnZIP_UnRAR_application
-This tool supports the Extraction of ZIP and RAR files Compressed without a Password and support for files compressed with Password along with ZIP and RAR Compression is coming in the future release.
+This tool supports **.zip**, **.rar** and **.7z** extraction. Support for Password protected compressed files is coming in the future release.
 
+## New Features!
+  - Support for **.7z** extraction added.
+  
 ## Installation
 1. Install [Python 3](https://www.python.org/downloads/)
-2. Use the package manager [pip](https://pip.pypa.io/en/stable/) to install [rarfile](https://rarfile.readthedocs.io/en/latest/api.html).
-
+2. Use the package manager [pip](https://pip.pypa.io/en/stable/) to install [rarfile](https://rarfile.readthedocs.io/en/latest/api.html), [py7zr](https://pypi.org/project/py7zr/) and [python-dotenv](https://pypi.org/project/python-dotenv/)
 ```bash
 pip install rarfile
+pip install py7zr
+pip install python-dotenv
 ```
-3. RAR file compression and uncompression is not directly possible with the [rarfile](https://rarfile.readthedocs.io/en/latest/api.html) library and requires additional downloads depending on the Operating System.
 
-- **For Windows:** Install [WinRAR](https://www.win-rar.com) (Either 32bit or 64bit). he PATH for WinRAR is already provided inside the constructor of ***"extract.py"*** which should suffice most of the Windows users.  Unless you have installed the 32-bit WinRAR on the 64-bit Windows Operating System, in this case, change the value of UNRAR_PATH in ***"extract.py"*** as follows:
-
-```python
-self.UNRAR_PATH = 'C:\\Program Files (x86)\\WinRAR\\UnRAR.exe'
+ 3. RAR file extraction is using [rarfile](https://rarfile.readthedocs.io/en/latest/api.html) requires additional downloads depending on the Operating System.
+- **For Windows:** Install [WinRAR](https://www.win-rar.com) (Either 32bit or 64bit). The PATH for WinRAR is already provided as an Environment variable: ***WINDOWS_OS_UNRAR_PATH = C:\\Program Files\\WinRAR\\UnRAR.exe*** in the ***".env"*** file which should work for most Windows OS users unless a 32-bit WinRAR version is installed on the 64-bit Windows Operating System, in this case, change the value of UNRAR_PATH in ***"env"*** as follows:
+```bash
+WINDOWS_OS_UNRAR_PATH = C:\\Program Files (x86)\\WinRAR\\UnRAR.exe
 ``` 
-- **For Mac:**  Install [HomeBrew](https://brew.sh/) and then [unrar](https://www.win-rar.com) as follows:
+- **For Mac:**  Install [HomeBrew](https://brew.sh/) and then [unrar](https://www.win-rar.com) with the following commands:
 ```bash
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew install unrar
 ``` 
-- **For Ubuntu:**  Install [unrar](https://www.win-rar.com) as follows:
+- **For Ubuntu:**  Install [unrar](https://www.win-rar.com) with the following command:
 ```bash
 sudo apt-get install -y rar unrar
 ``` 
 ## Usage
 1. Close the Repository.
 2. Install all the requirements listed in the earlier section.
-3. Delete the samples provided and copy all the compressed ***".zip"*** and ***".rar"*** files into the *"archives"* directory.
-    ###### NOTE: A few Samples of normal and corrupted ***".zip"*** and ***".rar"*** files are provided in the ***"archives"*** directory to test the tool for **Extraction** and **Exception handling**.
-4. The Uncompressed files will be generated under the ***"dumps"*** directory.
+3. Delete the samples provided and copy the compressed files into the ***"archives"*** directory.
+    ###### NOTE: A few Samples of normal and corrupted **.zip**, **.rar** and **.7z** files are provided in the ***"archives"*** directory to test **Extraction** and **Exception handling**.
+4. The ***"extracted"*** directory will contain all the extracted files under their respective sub directories.
 
 ## Running the application
 Navigate to the **compress_extract_tool** directory and run the following command:
 ```bash
 python index.py
 ```
-- ###### NOTE: The value for variable ***self.INITIAL_DIRECTORY_NAME*** is equal to ***1*** in the ***"extract.py"*** file which indicates that the first extracted archive will be stored in the directory ***"1"***. Similarly, if 10 archives are extracted 1o new sub-directories will be created under ***"dumps"*** directory with ***names: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10***.    
-```python 
-self.INITIAL_DIRECTORY_NAME = 1
+- ###### NOTE: The value for Environment variable: ***INITIAL_DIRECTORY_NAME = 1*** in the ***".env"*** indicates the first extracted archive will be stored in the sub-directory ***"1"*** under ***"extracted"*** directory. Similarly, if 10 archives are extracted to new sub-directories will be created under ***"extracted"*** directory with ***names: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10***.
+```bash
+INITIAL_DIRECTORY_NAME = 1
 ```
+
 ## License
 [MIT](https://choosealicense.com/licenses/mit/)
--- src/extract.py
+++ src/extract.py
@@ -1,61 +1,71 @@
 import zipfile    
-import rarfile              
+import rarfile
+import py7zr              
 import os
 import glob
 import sys
+import dotenv
+import shutil
 
 class Extract_archive:
     
     def __init__(self):
-        self.SOURCE_DIRECTORY = 'archives'
-        self.DESTINITION_DIRECTORY = 'dumps'
-        self.INITIAL_DIRECTORY_NAME = 1
-        self.UNRAR_PATH = 'C:\\Program Files\\WinRAR\\UnRAR.exe'    # Used when the script is running on Windows OS
-        self.EXCEPTIONS = dict()                                    # To store all the exceptions
-    
-    def unzip(self):
-        os.chdir(os.path.normpath(os.getcwd() + os.sep + self.SOURCE_DIRECTORY))  
+        dotenv.load_dotenv()
         
-        for file in glob.glob('*.zip'):
-            try:
-                with zipfile.ZipFile(file, 'r') as zipObj:
-                    zipObj.extractall(os.path.normpath(os.getcwd() + os.sep + '..' + os.sep + self.DESTINITION_DIRECTORY + os.sep + str(self.INITIAL_DIRECTORY_NAME)))
-                print(f'Extracted: {file} -----> {str(self.INITIAL_DIRECTORY_NAME)}')
-                self.INITIAL_DIRECTORY_NAME += 1
-            except:
-                print(f'\n*****\nALERT for FILE: {file}\nException: {sys.exc_info()[1]}\nException type: {sys.exc_info()[0]}\n*****\n')
-                self.EXCEPTIONS[f'File: {str(file)}'] = f'Exception type: {sys.exc_info()[0]}, {sys.exc_info()[1]}'
+        self.SOURCE_DIRECTORY = os.getenv("SOURCE_DIRECTORY")
+        os.chdir(os.path.normpath(os.getcwd() + os.sep + self.SOURCE_DIRECTORY))
 
-        os.chdir(os.path.normpath(os.getcwd() + os.sep + '..'))
-        
-    def unrar(self): 
-        os.chdir(os.path.normpath(os.getcwd() + os.sep + self.SOURCE_DIRECTORY)) 
-        
+        self.DESTINITION_DIRECTORY = os.getenv("DESTINITION_DIRECTORY")
+        self.INITIAL_DIRECTORY_NAME = int(os.getenv("INITIAL_DIRECTORY_NAME"))
+        self.WINDOWS_OS_UNRAR_PATH = os.getenv("WINDOWS_OS_UNRAR_PATH")
+        self.SUPPORTED_FORMATS = ['*.' + value for value in os.getenv("SUPPORTED_FORMATS").split(',')]
+        self.DELETE_AFTER_EXTRACTION = os.getenv("DELETE_AFTER_EXTRACTION")
+
+        self.EXCEPTIONS = dict()                                          
+
+    def extract(self):
         if os.name == 'nt':
-            rarfile.UNRAR_TOOL = self.UNRAR_PATH
-        
-        for file in glob.glob('*.rar'):
-            try:
-                with rarfile.RarFile(file, 'r') as rarObj:
-                    rarObj.extractall(os.path.normpath(os.getcwd() + os.sep + '..' + os.sep + self.DESTINITION_DIRECTORY + os.sep + str(self.INITIAL_DIRECTORY_NAME)))
-                print(f'Extracted: {file} -----> {str(self.INITIAL_DIRECTORY_NAME)}')
-                self.INITIAL_DIRECTORY_NAME += 1
-            except:
-                print(f'\n*****\nALERT for FILE: {file}\nException: {sys.exc_info()[1]}\nException type: {sys.exc_info()[0]}\n*****\n')
-                self.EXCEPTIONS[f'File: {str(file)}'] = f'Exception type: {sys.exc_info()[0]}, {sys.exc_info()[1]}'
-                  
-        os.chdir(os.path.normpath(os.getcwd() + os.sep + '..'))   
-        
+            rarfile.UNRAR_TOOL = self.WINDOWS_OS_UNRAR_PATH
+
+        print()
+        count = 1
+        for format in self.SUPPORTED_FORMATS:
+            for file in glob.glob(format):
+                try:
+                    if format == '*.zip':
+                        with zipfile.ZipFile(file, 'r') as zipObj:
+                            zipObj.extractall(os.path.normpath(os.getcwd() + os.sep + '..' + os.sep + self.DESTINITION_DIRECTORY + os.sep + str(self.INITIAL_DIRECTORY_NAME)))
+                    elif format == '*.rar':
+                        with rarfile.RarFile(file, 'r') as rarObj:
+                            rarObj.extractall(os.path.normpath(os.getcwd() + os.sep + '..' + os.sep + self.DESTINITION_DIRECTORY + os.sep + str(self.INITIAL_DIRECTORY_NAME)))
+                    elif format == '*.7z':
+                        with py7zr.SevenZipFile(file, 'r') as sevenZObj:
+                            sevenZObj.extractall(os.path.normpath(os.getcwd() + os.sep + '..' + os.sep + self.DESTINITION_DIRECTORY + os.sep + str(self.INITIAL_DIRECTORY_NAME)))
+                    print(f'{count}. FILE: {file} =====> {str(self.INITIAL_DIRECTORY_NAME)}\n----------------------------------------------------------------------------------------')
+                    self.INITIAL_DIRECTORY_NAME += 1
+                    count += 1
+
+                except:
+                    # print(f'\n*****\nALERT for FILE: {file}\nException: {sys.exc_info()[1]}\nException type: {sys.exc_info()[0]}\n*****\n')
+                    self.EXCEPTIONS[f'File: {str(file)}'] = f'Exception type: {sys.exc_info()[0]}, {sys.exc_info()[1]}'
+
     def check_exceptions(self):   
         if self.EXCEPTIONS:
-            print("\nExtraction complete with the following Exceptions:")
+            print("\nEXCEPTIONS:")
             for key, value in self.EXCEPTIONS.items():
-                print(f'{key} -----> {value}')
-            print("\n")
+                print(f'{key} =====> {value}')
+            print("\nEXTRACTION finished with exceptions.....")
         else:
-            print("\nExtraction complete.....\n")
+            if bool(self.DELETE_AFTER_EXTRACTION) == True:
+                self.delete_archive_helper()
+            print('\nEXTRACTION finished.....')
+
+    def delete_archive_helper(self):
+        try:
+            shutil.rmtree(os.getcwd())
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (os.getcwd(), e))
 
     def boot(self):
-        self.unzip()
-        self.unrar()
+        self.extract()
         self.check_exceptions()
